8d7a183aa6d5b6cc1ab360f4df4ac8e5bb675e73
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-13 09:35:38 +0200

    add html file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var firstNumber, secondNumber ,thirdNumber,formValues;
+// formValues=document.getElementById("form1").value;
+firstNumber=document.getElementById("one").value;
+secondNumber=document.getElementById("two").value;
+thirdNumber=document.getElementById("three").value;
+function tracker(){
+     if ((one===two)&&(two===three)){
+        alert("Triangle is equivalent");  
+     }
+     else {
+         alert("not equivalent")
+     }
+}
+
+//do commits
