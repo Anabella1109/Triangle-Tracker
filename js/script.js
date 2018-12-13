6230837f2841a1e2331f267b3de55e2753b19eb5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-13 09:51:56 +0200

    add function definition in js file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var firstNumber, secondNumber ,thirdNumber,formValues;
 
-
+function tracker(){
+     if ((one===two)&&(two===three)){
+        alert("Triangle is equivalent");  
+     }
+     else {
+         alert("not equivalent")
+     }
+}
 
 //do commits
