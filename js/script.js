d11e08284f6f33399e8b961433483521a3982b23
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-13 14:13:50 +0200

    change variable name

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 function tracker(){
-    var firstNumber, secondNumber ,thirdNumber, formValues ,text;
+    var firstNumber, secondNumber ,thirdNumber,output;
 // formValues=document.getElementById("form1").value;
 firstNumber=document.getElementById("one").value;
 secondNumber=document.getElementById("two").value;
@@ -8,20 +8,20 @@ thirdNumber=document.getElementById("three").value;
 
 
     if(firstNumber<=0||secondNumber<=0||thirdNumber<=0){
-        text="Please enter a positive number which is not zero";
+        output="Please enter a positive different from zero only";
     }
     else if(firstNumber===secondNumber&&secondNumber===thirdNumber){
-        text="This is an Equilateral triangle";
+        output="This is an Equilateral triangle";
     }
     else if(firstNumber===secondNumber||firstNumber===thirdNumber||secondNumber===thirdNumber){
-        text="This is an Isosceles triangle";
+        output="This is an Isosceles triangle";
     }
     else if(((firstNumber!==secondNumber)&&(secondNumber!==thirdNumber)&&(thirdNumber!==firstNumber))&&(((firstNumber + secondNumber) > thirdNumber) && ((firstNumber +thirdNumber) > secondNumber) &&((secondNumber + thirdNumber )> firstNumber))){
-        text="This is a Scalene triangle";
+        outputt="This is a Scalene triangle";
     }
     else {
         output="This is not a triangle";
     }
-    document.getElementById("result").innerHTML=text; 
+    document.getElementById("result").innerHTML=output; 
     document.getElementById("result").innerHTML+=" ";
 }
