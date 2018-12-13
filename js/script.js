52c73efed01fdf10b7b1fc01b874e0254782710c
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-13 09:53:47 +0200

    assign user input values to variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var firstNumber, secondNumber ,thirdNumber,formValues;
-
+formValues=document.getElementById("form1").value;
+firstNumber=document.getElementById("one").value;
+secondNumber=document.getElementById("two").value;
+thirdNumber=document.getElementById("three").value;
 function tracker(){
      if ((one===two)&&(two===three)){
         alert("Triangle is equivalent");  
