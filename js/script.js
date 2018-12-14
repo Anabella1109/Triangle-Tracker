c09d5dfbd35e196244ec124968474adf9c273f56
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-14 14:54:37 +0200

    add explanatory comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-
+//function definition
 function tracker(){
-    var firstNumber,secondNumber,thirdNumber,values,output;
-    
+    var firstNumber,secondNumber,thirdNumber,values; //declaring variables to store user inputs.
+    var output; //creating variable to store the output
     
 firstNumber=parseInt(document.getElementById("one").value);
 secondNumber=parseInt(document.getElementById("two").value);
-thirdNumber=parseInt(document.getElementById("three").value);
+thirdNumber=parseInt(document.getElementById("three").value);// storing user input in variables
 
-values=[firstNumber,secondNumber,thirdNumber];
+values=[firstNumber,secondNumber,thirdNumber];//putting variables in an array so they can  be turned in a string later
     if(firstNumber<=0||secondNumber<=0||thirdNumber<=0){
-        output="Please enter positive numbers different from zero only";
+        output="Please enter positive numbers different from zero only";//checking if user's inputs are valid
         
     }
     else if((firstNumber + secondNumber <= thirdNumber &&
-        secondNumber + firstNumber <= thirdNumber) ||
-      (firstNumber + thirdNumber <= secondNumber &&
+        secondNumber + firstNumber <= thirdNumber) ||                               
+      (firstNumber + thirdNumber <= secondNumber &&                         //checking if triangle can't be formed by checking if sum of two values is less or equal to third part
         thirdNumber + firstNumber <= secondNumber) ||
       (secondNumber + thirdNumber <= firstNumber && thirdNumber + secondNumber <= firstNumber)){
           output="Not a triangle";
@@ -22,15 +22,15 @@ values=[firstNumber,secondNumber,thirdNumber];
 
       }
   
-    else if(firstNumber===secondNumber&&secondNumber===thirdNumber&&thirdNumber===firstNumber){
-        output="An Equilateral triangle";
+    else if(firstNumber===secondNumber&&secondNumber===thirdNumber&&thirdNumber===firstNumber){              
+        output="An Equilateral triangle";  //checking if triangle is equilateral by checking if all sides are equal
     }
     else if(firstNumber===secondNumber||firstNumber===thirdNumber||secondNumber===thirdNumber){
-        output="An Isosceles triangle";
+        output="An Isosceles triangle";    //checking if triangle is isosceles by checking if any two sides are equal
     }
     else if((firstNumber!==secondNumber) &&
 
-      (firstNumber!==thirdNumber) &&
+      (firstNumber!==thirdNumber) &&            //checking if triangle is scalene by checking if no sides are equal
        
       (secondNumber!== thirdNumber)){
         output="A Scalene triangle";
@@ -38,8 +38,8 @@ values=[firstNumber,secondNumber,thirdNumber];
      else {
          output="error";
      }
-     document.getElementById("result").innerHTML="Sides entered("+values.join()+")"+" form: ";
-    document.getElementById("result").innerHTML+=output; 
+     document.getElementById("result").innerHTML="Sides entered("+values.join()+")"+" form: ";//changing array of inputs into string and and displaying it on webpage
+     document.getElementById("result").innerHTML+=output;  //displaying output 
     
     
 
