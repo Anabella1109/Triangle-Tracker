75f034d8bff75cb2db8b7b4e9890a3be177b8030
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-14 11:37:12 +0200

    edit function in javascript file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
 
 function tracker(){
     var firstNumber, secondNumber ,thirdNumber,output;
+    
+
 // formValues=document.getElementById("form1").value;
-firstNumber=document.getElementById("one").value;
-secondNumber=document.getElementById("two").value;
-thirdNumber=document.getElementById("three").value;
+firstNumber=parseInt(document.getElementById("one").value);
+secondNumber=parseInt(document.getElementById("two").value);
+thirdNumber=parseInt(document.getElementById("three").value);
 
 
     if(firstNumber<=0||secondNumber<=0||thirdNumber<=0){
         output="Please enter positive numbers different from zero only";
     }
-    else if(firstNumber===secondNumber&&secondNumber===thirdNumber){
+    else if((firstNumber + secondNumber <= thirdNumber &&
+        secondNumber + firstNumber <= thirdNumber) ||
+      (firstNumber + thirdNumber <= secondNumber &&
+        thirdNumber + firstNumber <= secondNumber) ||
+      (secondNumber + thirdNumber <= firstNumber && thirdNumber + secondNumber <= firstNumber)){
+          output="This is not a triangle";
+          
+
+      }
+  
+    else if(firstNumber===secondNumber&&secondNumber===thirdNumber&&thirdNumber===firstNumber){
         output="This is an Equilateral triangle";
     }
     else if(firstNumber===secondNumber||firstNumber===thirdNumber||secondNumber===thirdNumber){
         output="This is an Isosceles triangle";
     }
-    else if(((firstNumber!==secondNumber)&&(secondNumber!==thirdNumber)&&(thirdNumber!==firstNumber))&&(((firstNumber + secondNumber) > thirdNumber) && ((firstNumber +thirdNumber) > secondNumber) &&((secondNumber + thirdNumber )> firstNumber))){
+    else if((firstNumber!==secondNumber) &&
+
+      (firstNumber!==thirdNumber) &&
+       
+      (secondNumber!== thirdNumber)){
         output="This is a Scalene triangle";
     }
      else {
-         output="This is not a triangle";
+         output="error";
      }
     document.getElementById("result").innerHTML=output; 
-    document.getElementById("result").innerHTML+=" ";
 }
-//change the ask google in readme
+
